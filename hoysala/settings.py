f2f3377ec12ce41437710c610c212e1d48eda7b3
ChiranthAP <chiranthap27@gmail.com>
2025-05-13 14:43:37 +0530

    Switch to SQLite database

--- hoysala/settings.py
+++ hoysala/settings.py
@@ -67,10 +67,10 @@ WSGI_APPLICATION = 'hoysala.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
